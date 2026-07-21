8b6518996d88e79e9c4bed2685e2424cd02b287f
notgame-minecraft <werewolfoutside@gmail.com>
2026-07-20 22:15:30 -0400

    update

--- Build/game v2.loader.js
+++ Build/game v2.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65386434,
+    wasmFileSize: 65386570,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
